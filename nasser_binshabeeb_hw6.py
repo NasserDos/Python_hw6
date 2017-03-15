3e5b6e6f06ce1772f882d96385a29ec5377f9304
NasserDos <nasserbinshabeeb@mail.weber.edu>
2017-03-15 17:35:44 -0600

    initial commit

---
+++ nasser_binshabeeb_hw6.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+#################################################
+#
+# Name : Nasser Binshabeeb
+# 
+#
+# Project Info : Python
+#
+#
+#################################################
+
+#Main Function
+def main():
+    """
+    Main function
+    """
+    pass
+
+if __name__ == "__main__":
+    # Call Main
+    main()
+
+    exit(0)
+
+
+
+#exit(0)
+
